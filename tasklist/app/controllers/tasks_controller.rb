dc5897d7dc4ad31648475bcf97a4e68201ee30f1
fumiaki.yoshikawa <fumiaki@sunpow-er.co.jp>
2019-02-09 00:07:06 +0000

    bootstrap design

---
+++ tasklist/app/controllers/tasks_controller.rb
@@ -0,0 +1,70 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.all.page(params[:page]).per(25)
+  end
+  
+  def show
+    set_task
+    
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = '正常に投稿されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = '正常に更新されませんでした。'
+      render :new
+    end
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def edit
+    set_task
+  end
+  
+  def delete
+    set_task
+    @task.destroy
+    
+    flash[:success] = '正常に削除されました。'
+    redirect_to tasks_url
+  end
+  
+  def destroy
+    set_task
+    @task.destroy
+    
+    flash[:success] = '正常に削除されました。'
+    redirect_to tasks_url
+  end
+  
+  def update
+    set_task
+    
+    if @task.update(task_params)
+      flash[:success] = '正常に更新されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = '正常に更新できませんでした。'
+      render :edit
+    end
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :title)
+  end
+end
--- tasklist/db/seeds.rb
+++ tasklist/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..300).each do |number|
+  Task.create(title:'Test Title' + number.to_s, content: 'TEST CONTENT' + number.to_s)
+end
